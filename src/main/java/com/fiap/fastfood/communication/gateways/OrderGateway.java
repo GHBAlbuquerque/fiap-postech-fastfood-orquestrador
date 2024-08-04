5e0da93ad2070a12e9c99f3d40ded2f40e9aec0b
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2024-08-04 18:15:54 -0300

    feature: mapeia filas nas propriedades

--- src/main/java/com/fiap/fastfood/communication/gateways/OrderGateway.java
+++ src/main/java/com/fiap/fastfood/communication/gateways/OrderGateway.java
@@ -1,9 +1,15 @@
 package com.fiap.fastfood.communication.gateways;
 
+import io.awspring.cloud.sqs.annotation.SqsListener;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class OrderGateway {
 
     private static final Logger logger = LogManager.getLogger(OrderGateway.class);
+
+    @SqsListener({"${my.queue.url}", "myOtherQueue"})
+    public void listenTwoQueues(String message) {
+        System.out.println(message);
+    }
 }
